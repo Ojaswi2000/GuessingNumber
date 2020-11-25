b6bc745fdc22ca9ad24a5b0a5603e36827f02fd3
Ojaswi Awasthi <65769340+Ojaswi2000@users.noreply.github.com>
2020-11-26 02:20:27 +0530

    rendered the GameOverScreen by updating rounds in the game

--- App.js
+++ App.js
@@ -1,27 +1,34 @@
 import React,{useState} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import Header from './components/Header';
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
+import GameOverScreen from './screens/GameOverScreen';
 export default function App() {
 
   const [userNumber,setUserNumber]=useState();
+  const [guessRounds,setGuessRounds]=useState(0);
 
   const startGameHandler = (selectedNumber)=>{
     setUserNumber(selectedNumber);
-
+    setGuessRounds(0);
   };
-  let content = <StartGameScreen onStartGame={startGameHandler}/>;
+  const GameOverHandler=numOfRounds=>{
+    setGuessRounds(numOfRounds);
+  };
+  let content = <StartGameScreen onStartGame={startGameHandler} />;
 
-  if(userNumber)
+  if(userNumber && guessRounds<=0)
   {
-    content =<GameScreen userChoice={userNumber}/>;
+    content =<GameScreen userChoice={userNumber} onGameOver={GameOverHandler} />;
   }
+  else if(guessRounds>0)
+  content=<GameOverScreen />;
 
   return (
     <View style={styles.screen}>
       <Header title='Guess A number'/>
-      {content};
+      {content}
     </View>
   );
 }
